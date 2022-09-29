0724b88b44511e07f2cb693d7ff2112d10b94e7a
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2022-09-29 13:36:22 +0800

    delopy

--- assets/Detail.9db48834.js
+++
@@ -1,9 +0,0 @@
-import{a as d}from"./BaseBtn.25617df1.js";import{o as c,c as r,b as l,a as t,d as o}from"./index.e6e197a9.js";const i=(s,e)=>{const n=s.__vccOpts||s;for(const[a,p]of e)n[a]=p;return n},_={},u={class:"inline-block pr-5 mt-5 leading-snug bg-neutral-800 text-amber-500"},f=l(`<pre><code>\r
-   <span class="text-indigo-500">&lt;<span class="text-rose-500">BaseBtn</span></span>\r
-   \r
-     <span>before-icon<span class="text-sky-500">=</span><span class="text-teal-500">&quot;cog&quot;</span></span>\r
-     <span>label<span class="text-sky-500">=</span><span class="text-teal-500">&quot;button&quot;</span></span>\r
-     <span>rounded<span class="text-sky-500">=</span><span class="text-teal-500">&quot;rounded-md&quot;</span></span>\r
-   <span class="text-indigo-500">/&gt;</span>\r
-</code>\r
-</pre>`,1),x=[f];function b(s,e){return c(),r("div",u,x)}const m=i(_,[["render",b]]),h=l('<nav class="fixed flex flex-col h-full p-4 space-y-4 font-semibold text-white bg-slate-600"><a href="#color">Color</a><a href="#fontSize">fontSize</a><a href="#icon">Icon</a><a href="#button">Button</a></nav>',1),g={class:"p-16 ml-20"},B={class:"pb-4 border-b border-solid border-neutral-500"},k=t("h1",{class:"mb-4 text-3xl font-semibold"}," Button ",-1),v={class:"flex items-center space-x-6"},$={__name:"Detail",setup(s){return(e,n)=>{const a=d;return c(),r("main",null,[h,t("div",g,[t("section",B,[k,t("div",v,[o(a,{"before-icon":"cog",label:"button",rounded:"rounded-md"}),o(m)])])])])}}};export{$ as default};
